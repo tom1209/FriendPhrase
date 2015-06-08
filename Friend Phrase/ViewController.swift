8dd00ad28bb6e15710b7a3d326908e189e56ff78
tom1209 <tomwhite1209@gmail.com>
2015-06-08 15:01:45 -0400

    Initial Commit

---
+++ Friend Phrase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Friend Phrase
+//
+//  Created by Thomas White on 2015-06-08.
+//  Copyright (c) 2015 Thomas White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
