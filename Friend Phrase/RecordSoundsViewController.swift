826bebefe4a07a696c5188957c31c4e02381cb52
tom1209 <tomwhite1209@gmail.com>
2015-06-17 18:21:28 -0400

    Just an update before dinner, trying to get audio to work

---
+++ Friend Phrase/RecordSoundsViewController.swift
@@ -0,0 +1,79 @@
+//
+//  RecordSoundsViewController.swift
+//  Friend Phrase
+//
+//  Created by Thomas White on 2015-06-08.
+//  Copyright (c) 2015 Thomas White. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var microphone: UIButton!
+    
+    var audioRecorder : AVAudioRecorder!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        //Hide stop button
+        stopButton.hidden = true
+        //Enable record at start
+        microphone.enabled = true
+    }
+
+
+    @IBAction func stopRecording(sender: UIButton) {
+        //TODO: stop recording
+        recordingInProgress.hidden = true
+        //Stop audio recorder
+        audioRecorder.stop()
+        var audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, error: nil)
+    }
+    
+    @IBAction func recordAudio(sender: UIButton) {
+        stopButton.hidden = false
+        recordingInProgress.hidden = false
+        microphone.enabled = false
+        //println("in recordAudio")
+        
+        //TODO: record user's voice
+        //Newly Added to actually record
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
+        
+        let recordingName = "my_audio.wav"
+        var pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        println(filePath)
+        
+        //Setup audio session
+        var session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error:nil)
+        //Initialize and prepare the recorder
+        audioRecorder = AVAudioRecorder(URL: filePath, settings:nil, error:nil)
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool)
+    {
+        //TODO: save recorded audio
+        
+        //TODO: move to second scene of the app
+    }
+}
+
